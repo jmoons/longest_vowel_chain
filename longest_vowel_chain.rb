c87af8053c0f2d4e496863c9c369108d971c8812
John Moons <j.moons@realdigitalmedia.com>
2019-09-23 14:03:12 -0400

    Eliminate second iteration loop, just keep track of longest vowel sequence in the first loop

--- longest_vowel_chain.rb
+++ longest_vowel_chain.rb
@@ -1,7 +1,7 @@
 VOWELS = ["a", "e", "i", "o", "u"]
 
 test_word                 = "chocolicious"
-vowel_chain_array         = []
+longest_vowel_chain_array = []
 working_vowel_chain_array = []
 
 test_word.split('').each do | test_character |
@@ -9,17 +9,12 @@ test_word.split('').each do | test_character |
   if VOWELS.include?( test_character )
     working_vowel_chain_array << test_character
   else
-    vowel_chain_array << working_vowel_chain_array if working_vowel_chain_array.length > 0
+    longest_vowel_chain_array = working_vowel_chain_array if working_vowel_chain_array.length > longest_vowel_chain_array.length
     working_vowel_chain_array = []
   end
 
 end
 
-longest_vowel_chain = []
-vowel_chain_array.each do | vowel_chain |
-  longest_vowel_chain = vowel_chain if vowel_chain.length > longest_vowel_chain.length
-end
-
 puts "Test Word:                  #{test_word}"
-puts "Longest Vowel Chain:        #{longest_vowel_chain.inspect}"
-puts "Longest Vowel Chain Length: #{longest_vowel_chain.length.inspect}"
+puts "Longest Vowel Chain:        #{longest_vowel_chain_array.inspect}"
+puts "Longest Vowel Chain Length: #{longest_vowel_chain_array.length.inspect}"
